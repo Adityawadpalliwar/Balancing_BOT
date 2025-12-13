ce16f02161bc7f113c26e4360476e7082d2d6c2d
Aditya wadpalliwar <91073666+Adityawadpalliwar@users.noreply.github.com>
2025-12-13 17:25:09 +0530

    imu checked

--- using_imu/using_imu.ino
+++ using_imu/using_imu.ino
@@ -1,12 +1,7 @@
 #include <Wire.h>
 #include <MPU6050_light.h>
 MPU6050 mpu(Wire);
-float angleZeroX;
-float angleZeroY;
-float angleZeroZ;
-float gyroOffsetX;
-float gyroOffsetY;
-float gyroOffsetZ;
+
 
 
 void setup(){
@@ -17,29 +12,19 @@ void setup(){
   //mpu.setFilterGyroCoef(0.95);(change if wanted)
   //mpu.setFilterAccCoef(0.02);
   mpu.calcOffsets();
-  angleZeroX = mpu.getAngleX();
-  angleZeroY = mpu.getAngleY();
-  angleZeroZ = mpu.getAngleZ();
-  gyroOffsetX = mpu.getGyroX();
-  gyroOffsetY = mpu.getGyroY();
-  gyroOffsetZ = mpu.getGyroZ();
+ 
 }
 void loop(){
   mpu.update();
-  float angle[3] ={mpu.getAngleX() - angleZeroX,mpu.getAngleY()- angleZeroY,mpu.getAngleZ()- angleZeroZ};
-  float gyro[3] = {mpu.getGyroX()-gyroOffsetX,mpu.getGyroY()-gyroOffsetY,mpu.getGyroZ()-gyroOffsetZ};
-  Serial.println("angle along x is :");
-  Serial.print(angle[0]);
-  Serial.println("angle along y is :");
-  Serial.print(angle[1]);
-  Serial.println("angle along z is :");
-  Serial.print(angle[2]);
-  Serial.println("velocity along x is :");
-  Serial.print(gyro[0]);
-  Serial.println("velocity along y is :");
-  Serial.print(gyro[1]);
-  Serial.println("velocity along z is :");
-  Serial.print(gyro[2]);
+  float angle = mpu.getAngleY();
+  float gyro = mpu.getGyroY();
+  delay(100);
+  Serial.print("angle along y is : ");
+  Serial.println(angle);
+  delay(100);
+  Serial.print("velocity along y is :");
+  Serial.println(gyro);
+  
 
   return 0;
   

f46424abe7d70c6c36460b41cd41e1f1633b7642
Aditya wadpalliwar <91073666+Adityawadpalliwar@users.noreply.github.com>
2025-12-14 15:21:44 +0530

    now motor is given angular velocity

--- draft1/draft1.ino
+++ draft1/draft1.ino
@@ -124,11 +124,12 @@ void stopMotors()
 void computeLQRControl() {
   
   float U_balance = -(K1 * x1 + K2 * x2 + K3 * x3 + K4 * x4);
+
   
   // Convert control signal to PWM (scale appropriately)
-  float pwm_scale = 10.0;  // Tune this accordingly
-  float U_right = U_balance * pwm_scale; //(add forward and backward velocity here itself)
-  float U_left = U_balance * pwm_scale;
+  //float pwm_scale = 10.0;  // Tune this accordingly
+  float U_right = (U_balance)*51.0/0.022 ; //(add forward and backward velocity here itself)
+  float U_left = (U_balance)*51/0.022 ;
   
   // Safety check: if robot is too tilted, stop motors
   if (abs(x2) > 22) {  // ~45 degrees
